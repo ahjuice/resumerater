b50a3749a787e1363ca02427f5b1e4ce2c7ad1ff
ahjuice <ahjuice@users.noreply.github.com>
2018-06-18 17:04:41 -0400

    fixed some URLs in adapter

--- frontend_resume-rater/src/adapter.js
+++ frontend_resume-rater/src/adapter.js
@@ -1,5 +1,5 @@
-const base_url = "http://localhost:3000/api/v1"
-const resumeURL = "http://localhost:3000/api/v1/resumes"
+const baseURL = "http://localhost:3000/api/v1"
+const resumeURL = `${baseURL}/resumes`
 
 class Adapter {
   static getResumes(){
