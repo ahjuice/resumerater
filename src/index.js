16284eb58a45e60885e991ad857558f0bd1a77b1
teasacura <teasacuraster@gmail.com>
2018-06-19 16:45:37 -0400

    Forms are done

--- src/index.js
+++ src/index.js
@@ -2,35 +2,7 @@ document.addEventListener('DOMContentLoaded', init)
 
 
 function init(){
-  Adapter.getResumes()
-    .then()
-  const form = FormBuilder.createUser()
-  document.body.innerHTML = form
-
-  let userForm = document.querySelector("#create-user-form")
-
-  userForm.addEventListener("submit", function(e) {
-    e.preventDefault()
-    let nameValue = document.querySelector("#user-name-input").value
-    let emailValue = document.querySelector("#email-input").value
-
-    let data = {
-      name: nameValue,
-      email: emailValue
-    }
-
-    Adapter.createUser(data)
-      .then(console.log)
-  })
 }
-
-
-// function renderResumeForm(){
-//   console.log(FormBuilder.createUser())
-//   // document.body.prepend(form)
-// }
-
-// renderResumeForm()
 
 // function renderImage(arr){
 //   let imageDiv = document.querySelector(".image-div")
