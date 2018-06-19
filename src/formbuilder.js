4744c4d74d087edecda710cb02aa72813087acb7
teasacura <teasacuraster@gmail.com>
2018-06-19 15:40:24 -0400

    update

--- src/formbuilder.js
+++ src/formbuilder.js
@@ -1,3 +1,25 @@
 class FormBuilder {
-  
+  static createUser(){
+    return `
+    <form action="#" method="post">
+      Name:<br>
+      <input type="text" name="name"><br>
+      Email:<br>
+      <input type="text" name="email">
+      <input type="submit" value="Submit"
+    </form>
+    `
+  }
+
+  static createResume(){
+    return `
+    <form action="#" method="post">
+      Name:<br>
+      <input type="text" name="name"><br>
+      Email:<br>
+      <input type="text" name="email">
+      <input type="submit" value="Submit"
+    </form>
+    `
+  }
 }
