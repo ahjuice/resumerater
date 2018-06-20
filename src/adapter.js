d82a54864893098f65bc18fe9c79e0795618c165
jessebillard <jessebillard@mac.com>
2018-06-20 10:04:07 -0400

    adds working login adapter method

--- src/adapter.js
+++ src/adapter.js
@@ -65,6 +65,18 @@ class Adapter {
     return fetch(`${resumesURL}/${resumeId}/comments`, options).then(r => r.json())
   }
 
+  static login(email) {
+    const jsonEmail = JSON.stringify(email)
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: jsonEmail
+    }
+    return fetch(`${baseURL}/login`, options).then(r => r.json())
+  }
+
   //This update method will be for future use once we have a backend controller action and html edit form rendered
   // static updateComment(resumeId, commentId, data) {
   //   const jsonCommentData = JSON.stringify(data)
