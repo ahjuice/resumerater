79ece1299726cd0aa950bef38222f4fa8c2b55b9
ahjuice <ahjuice@users.noreply.github.com>
2018-06-22 10:34:32 -0400

    fixed navbar variables

--- src/navbar.js
+++ src/navbar.js
@@ -20,13 +20,15 @@ const Navbar = function() {
     }
 
     static noUser() {
-      const loginLink = document.createElement("a");
-      loginLink.href = "#"
-      loginLink.innerHTML = "Log In"
-      loginLink.addEventListener('click', (e) => {
-        e.preventDefault();
-        View.render('login');
-      });
+      // const loginLink = document.createElement("a");
+      // loginLink.href = "#"
+      // loginLink.innerHTML = "Log In"
+      // loginLink.addEventListener('click', (e) => {
+      //   e.preventDefault();
+      //   View.render('login');
+      // });
+
+      const loginLink = Navbar.buildLink("Log In", View.render, ["login"]);
 
       const li = document.createElement("li");
       li.appendChild(loginLink);
@@ -35,18 +37,31 @@ const Navbar = function() {
     }
 
     static standardUser() {
-      const profileLink = document.createElement("a");
-      profileLink.href = "#"
-      profileLink.innerHTML = "My Profile"
-      profileLink.addEventListener('click', (e) => {
-        e.preventDefault();
-        View.checkForResumes(currentUser);
-      });
+      // const profileLink = document.createElement("a");
+      // profileLink.href = "#"
+      // profileLink.innerHTML = "My Profile"
+      // profileLink.addEventListener('click', (e) => {
+      //   e.preventDefault();
+      //   View.checkForResumes(currentUser);
+      // });
+
+      const profileLink = Navbar.buildLink("My Profile", View.checkForResumes, [currentUser]);
 
       const li = document.createElement("li");
       li.appendChild(profileLink);
 
       userLink.appendChild(li);
+    }
+
+    static buildLink(text, listenerFn, argumentsArr) {
+      const link = document.createElement("a");
+      link.href = "#"
+      link.innerHTML = text
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        listenerFn(...argumentsArr);
+      });
+      return link;
     }
   }
 }();
